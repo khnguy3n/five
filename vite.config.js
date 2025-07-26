d5e62319bad1c38725296814458bd1760523c22a
Khanh Nguyen <khnguy3n@proton.me>
2025-07-26 15:31:46 -0400

    create components folder setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: "/five/",
+  resolve: {
+    alias: {
+	'@components': '/src/components',
+    },
+  },
   server: {
     open: true,
     port: 8080,
