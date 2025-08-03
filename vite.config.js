2a3e9e246f9dc313b00aa5fccafecf1288e6f49a
Khanh Nguyen <cammini@Khanhs-Mac-mini.local>
2025-08-03 11:45:21 -0400

    format

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-  base: "/five/",
-  resolve: {
-    alias: {
-	'@components': '/src/components',
-    },
-  },
-  server: {
-    open: true,
-    host: '0.0.0.0',
-    port: 8080,
-  },
-})
+	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	base: "/five/",
+	resolve: {
+		alias: {
+			"@components": "/src/components",
+		},
+	},
+	server: {
+		open: true,
+		host: "0.0.0.0",
+		port: 8080,
+	},
+});
